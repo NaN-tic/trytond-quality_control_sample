600272abf0075e71ffb02623a10c010fdd6bd979
Raimon Esteve <raimonesteve@gmail.com>
2026-04-02 13:04:42 +0200

    Label Dominate report #183449

--- quality.py
+++ quality.py
@@ -1,14 +1,17 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms. """
 import datetime
-from trytond.model import Workflow, ModelView, ModelSQL, fields
-from trytond.pyson import Eval
-from trytond.transaction import Transaction
-from trytond.pool import Pool, PoolMeta
-from trytond.modules.jasper_reports.jasper import JasperReport
 from functools import reduce
 
-__all__ = ['Template', 'Sample', 'SampleReport']
+from dominate.tags import div, img, span
+
+from trytond.model import Workflow, ModelView, ModelSQL, fields
+from trytond.modules.html_report.dominate_report import DominateReport
+from trytond.pool import Pool, PoolMeta
+from trytond.pyson import Eval
+from trytond.tools import file_open
+from trytond.transaction import Transaction
+
 
 STATES = {
     'readonly': Eval('state') == 'done',
@@ -109,7 +112,7 @@ class Sample(Workflow, ModelSQL, ModelView):
         sequence = Config(1).sample_sequence
         for value in vlist:
             if not value.get('code'):
-                value['code'] = sequence.get()
+                value['code'] = sequence and sequence.get()
         return super(Sample, cls).create(vlist)
 
     @classmethod
@@ -122,6 +125,65 @@ class Sample(Workflow, ModelSQL, ModelView):
         return super(Sample, cls).copy(samples, default=default)
 
 
-class SampleReport(JasperReport):
+class SampleReport(DominateReport):
     'Sample Report'
     __name__ = 'quality.sample.report'
+    _single = True
+    side_margin = 0
+
+    @classmethod
+    def css(cls, action, data, records):
+        with file_open('quality_control_sample/label.css') as css_file:
+            return css_file.read()
+
+    @classmethod
+    def css_body(cls, action, data, records):
+        return cls.css(action, data, records) or ''
+
+    @classmethod
+    def header(cls, action, data, records):
+        return None
+
+    @classmethod
+    def footer(cls, action, data, records):
+        return None
+
+    @classmethod
+    def _tests_text(cls, sample):
+        values = []
+        for test in sample.tests:
+            value = (
+                test.render.external_description
+                if test.raw.external_description else
+                test.render.internal_description
+                if test.raw.internal_description else
+                test.render.number
+                if test.raw.number else '')
+            if value:
+                values.append(value)
+        return ', '.join(values)
+
+    @classmethod
+    def body(cls, action, data, records):
+        sample, = records
+        container = div(cls='label')
+        with container:
+            with div(cls='label-row label-main'):
+                span(sample.product.render.rec_name, cls='label-product')
+                tests_text = cls._tests_text(sample)
+                if tests_text:
+                    span(tests_text, cls='label-tests')
+            if sample.raw.lot:
+                div(sample.lot.render.number, cls='label-row label-lot')
+            if sample.render.barcode:
+                img(
+                    cls='label-barcode',
+                    src=cls.barcode(
+                        'ean13',
+                        sample.render.barcode,
+                        module_height=7.5,
+                        quiet_zone=0.8,
+                        font_size=9,
+                    ),
+                )
+        return container
